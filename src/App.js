73e9b825c90123f65321b9c2c7ff3ce9d58939d9
coole133 <vlad.yaromiy@gmail.com>
2020-07-06 21:35:36 +0300

    Ubuntu versioned

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React  from 'react';
-import HomePage from "./pages/HomePage/HomePage";
+import HomePage from "./Pages/HomePage/HomePage";
 import {Switch  , Route,} from "react-router-dom"
 import NavigationButtons from "./components/NavigationButtons/NavigationButtons";
-import MovieSearchPage from "./pages/MovieSearchPage/MovieSearchPage";
-import FavouritesPage from "./pages/FavouritesPage/FavouritesPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage"
+import MovieSearchPage from "./Pages/MovieSearchPage/MovieSearchPage";
+import FavouritesPage from "./Pages/FavouritesPage/FavouritesPage";
+import MovieDetailsPage from "./Pages/MovieDetailsPage/MovieDetailsPage"
 import Favourite from "./components/FavouritesMovies/FavouriteMenuButton";
 
 
